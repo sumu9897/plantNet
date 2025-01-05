895c0a197922374b3ac6239cf5fd18579b639214
sumu9897 <sumon.bncd@gmail.com>
2025-01-05 11:37:00 +0600

    Database SsetUp

--- server/index.js
+++ server/index.js
@@ -35,8 +35,7 @@ const verifyToken = async (req, res, next) => {
     next()
   })
 }
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mq0mae1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmec6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
